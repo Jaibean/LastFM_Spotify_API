0c45283d10b1a3cf9555565266c016c9bb0421d0
Jaibean <jaimie.bertoli@gmail.com>
2021-04-13 20:41:37 -0400

    secrets
    update key

--- lastfm-spotify/secrets.py
+++ lastfm-spotify/secrets.py
@@ -2,11 +2,10 @@ def spotify_user_id():
     return 'jbertoli'
 
 
+# changes every 20 minutes if you don't use. POST playlist
 def spotify_token():
-    return 'BQDBcV9YufafoNXlXC9I7NePkQB-mYGnHGH2Rh4-dW3ZSKSldYVSj9HSQfgcL8oHJ064nXWMt98kkq5AWH1GOenk5WOYdyHDficWU8n6klVqkPY1WE0OuWeffH5-sqbK-r4SZW9vlKpBFMdnQXb9k1ygFC1CiDjWNC1qMu5wQk4F'
+    return 'BQBvU5CEKvtJEFJCdNbZb3oxZppwEZzJ6phwa_8xrkAasjPpNcVSV63wmgJDUREOwxNmUSMtHdcVnTOVeaHR8MvQoFVh2-66AMKqjDvBvJE-PQmVMNnNQ38OU197WQp6OZ0IxDDdsXqjgtAewg_-6gxU-l8kRg1gPhdnWsFDdqhQ'
 
 
 def last_fm_api_key():
-    return '8c5cb39b7fee12698cad5bf87cbc3386'
-
-
+    return ''
