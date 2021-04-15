f4a8c29794f9f5fd79b05cb5636bdc38f932dfd8
Jaibean <jaimie.bertoli@gmail.com>
2021-04-14 21:04:09 -0400

    front end with flask
    adding styling to UI

--- lastfm-spotify/app.py
+++ lastfm-spotify/app.py
@@ -1,3 +1,4 @@
+from flask import Flask, render_template
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,8 +6,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template("index.html")
+
+@app.route('/name/<user>')
+def hello_user(user):
+    return render_template("index.html", user=user)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
